49570d408511f8092b0eb53896497f8832aee62f
Kidinnu <kidinnu@hotmail.com>
2023-05-25 07:06:29 +0400

    start

--- atm_data.m
+++ atm_data.m
@@ -1,4 +1,5 @@
-function [a,rho] = atm_data(height)
-    [~, a, ~, rho] = atmosisa(height);
+function [a,rho] = atm_data(height)    
+    [T, rho] = atmosnrlmsise00(height,0,0,2023,30,0);
+    rho = rho(6);
+    a = 300;
 end
-
--- main.m
+++ main.m
@@ -10,37 +10,38 @@ res = [];
 
 clc;
 dt = 30;
+t1 = 200;
 
 for gamma = linspace(0,2*pi,32)
 
-p.Is = @(P) 3000;
+    p.Is = @(P) 3000;
 
-c.alpha = @(t,q,p) (t>200 & t < 200+dt)*0.5;
-c.gamma = @(t,q,p) (t>200 & t < 200+dt)*gamma;
-c.P     = @(t,q,p) (t>200 & t < 200+dt)*1000000;
+    c.alpha = @(t,q,p) (t>t1 & t < t1+dt)*0.5;
+    c.gamma = @(t,q,p) (t>t1 & t < t1+dt)*gamma;
+    c.P     = @(t,q,p) (t>t1 & t < t1+dt)*100000*9.807;
 
-r0      = p.Rz + 60e3;
-v0      = 2200;
-theta0  = 30*pi/180;
-psi0    = 1;
-phi0    = 0;
-lambda0 = 0;
-m0      = 45000;
-q0      = [r0;v0;theta0;psi0;phi0;lambda0;m0];
+    r0      = p.Rz + 70e3;
+    v0      = 2200;
+    theta0  = 30*pi/180;
+    psi0    = 0;
+    phi0    = 0;
+    lambda0 = 0;
+    m0      = 48000;
+    q0      = [r0;v0;theta0;psi0;phi0;lambda0;m0];
 
-opt     = odeset('RelTol',1e-7,'AbsTol',1e-6,'Events',@(t,q) event_function(t,q,p,c));
-[t,q]   = ode113(@(t,q) yar_dqdt(t,q,p,c),[0 300], q0, opt);
+    opt     = odeset('RelTol',1e-7,'AbsTol',1e-6,'Events',@(t,q) event_function(t,q,p,c));
+    [t,q]   = ode45(@(t,q) yar_dqdt(t,q,p,c),[0 500], q0, opt);
 
-plot(t,(q(:,1)-p.Rz)*0.001);
+    plot(t,(q(:,1)-p.Rz)*0.001);
 
-vec_r0   = sph2ecef(lambda0,phi0,p.Rz);
-DCMnorth = pp2DCM(vec_r0, [0;0;p.Rz], p.Rz);
-DCMr0v0  = DCMnorth*Ay(psi0-pi/2);
+    vec_r0   = sph2ecef(lambda0,phi0,p.Rz);
+    DCMnorth = pp2DCM(vec_r0, [0;0;p.Rz], p.Rz);
+    DCMr0v0  = DCMnorth*Ay(psi0-pi/2);
 
-vec_re   = sph2ecef(q(end,6),q(end,5),p.Rz);
-atct     = sph2atct(vec_r0,DCMr0v0,vec_re,p.Rz);
+    vec_re   = sph2ecef(q(end,6),q(end,5),p.Rz);
+    atct     = sph2atct(vec_r0,DCMr0v0,vec_re,p.Rz);
 
-res      = [res atct];
+    res      = [res atct];
 
 end
 
